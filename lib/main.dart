50be8503701823924bfac4e2f5ec5513538deae4
beslimir <marin.beslo@gmail.com>
2022-12-08 10:15:58 +0100

    Row

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MaterialApp(
-  home: Home(),
-));
+      home: Home(),
+    ));
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -15,12 +15,27 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Padding(
-        padding: EdgeInsets.all(30.0),
-        child: Text('Hello'),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text('Hello :)'),
+          TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                  foregroundColor: Colors.grey,
+                  backgroundColor: Colors.red
+              ),
+              child: Text('Henlo frens')),
+          Container(
+            color: Colors.blue[200],
+            padding: EdgeInsets.all(30.0),
+            child: Text('Inside the container'),
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
+        onPressed: () {},
         backgroundColor: Colors.red[600],
         child: const Text('Click'),
       ),
